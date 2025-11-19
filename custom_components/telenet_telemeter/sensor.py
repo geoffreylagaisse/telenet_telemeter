106794d0edb5856e77de51289345b8272f6ddd2a
Glenn Carremans <me@glennmen.dev>
2025-11-19 19:55:34 +0100

    Fix bundle product check logic in get_desired_internet_product
    Apply code review feedback

--- custom_components/telenet_telemeter/sensor.py
+++ custom_components/telenet_telemeter/sensor.py
@@ -137,7 +137,7 @@ def get_desired_internet_product(products, desired_product_type):
     # Try to find a product with productType = "bundle"
     _LOGGER.debug(f'products: {products}, {desired_product_type}')
     bundle_product = next((product for product in products if product.get('productType','').lower() == desired_product_type), None)
-    if desired_product_type == 'bundle' and not bundle_product.get('products'):
+    if desired_product_type == 'bundle' and bundle_product and not bundle_product.get('products'):
         bundle_product = None
     _LOGGER.debug(f'desired_product: {bundle_product}, {desired_product_type}')
     
